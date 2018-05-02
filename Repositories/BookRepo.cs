2e5914253ca55ab7aa5a3045ae59930012d6f7be
Haddib <hb150794@gmail.com>
2018-05-02 15:22:58 +0000

    BookService og BookRepo bætt við

---
+++ Repositories/BookRepo.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using BookCave.Models.ViewModels;
+
+namespace BookCave.Repositories
+{
+    public class BookRepo
+    {
+        /*List<BookView> GetTop10BooksFromDB()
+        {
+            LINQ sem nær í BookEntity úr DB og breytir í BookView.
+        } */
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using BookCave.Models.ViewModels;
+
+namespace BookCave.Services
+{
+    public class BookServices
+    {
+        
+        /*List<BookView> GetTop10HighestRated()
+        {
+            var top10Books = Book
+            return top10Books;
+        }*/
+    }
+}
