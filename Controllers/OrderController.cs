50d56308b51081fe9c29caae7dc7e7d4e8f97806
SnaebjornB <34096981+SnaebjornB@users.noreply.github.com>
2018-05-03 17:39:23 +0000

    Add to basket og get basket föll

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using BookCave.Models.EntityModels;
 using BookCave.Models.ViewModels;
 using BookCave.Services;
@@ -17,19 +18,32 @@ namespace BookCave.Controllers
         [HttpGet]
         public IActionResult Basket()
         {
-            Basket(5, 2);
+            Basket(4);
             return View(orderBasketView);
         }
 
         [HttpPost]
-        public IActionResult Basket(int bookID, int quantity)
+        public IActionResult Basket(int customerID)
         {
-            var tempItem = new OrderItemEntity();
-            tempItem = orderService.getItem(bookID, quantity);
-            orderBasketView.books.Add(tempItem);
-            orderBasketView.totalPrice += tempItem.price * quantity;
+            orderBasketView = orderService.getBasket(customerID);
 
             return View(orderBasketView);
         }
+
+        [HttpPost]
+        public void addToBasket(int bookID, int quantity, int customerID)
+        {
+            orderService.addToBasket(bookID, quantity, customerID);
+        }
     }
 }
+                    /*public HttpResponseMessage addToBasket(int bookID, int quantity)
+                    {
+                        var tempItem = new OrderItemEntity();
+                        tempItem = orderService.getItem(bookID, quantity);
+                        orderBasketView.books.Add(tempItem);
+                        orderBasketView.totalPrice += tempItem.price * quantity;
+
+                        return View(orderBasketView);
+                    */
+    
