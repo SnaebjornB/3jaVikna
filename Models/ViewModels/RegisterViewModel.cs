247d656b4ea398f54254f5300a3ef1f25b6026ee
Haddib <hb150794@gmail.com>
2018-05-04 11:15:31 +0000

    Innskráningarkerfi

---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookCave.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please enter a valid email address.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Enter a valid password.")]
+        public string Password { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookCave.Data;
+using BookCave.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,6 +25,36 @@ namespace BookCave
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AuthenticationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthenticationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            services.Configure<IdentityOptions>(options =>
+            {
+                // User settings
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                //Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromHours(3);
+                //If the login path isn't set, ASP.NET Core defaults
+                //the path to /Account/Login
+                options.LoginPath = "/Account/Login";
+                //If the AccessDeniedPath isn't set, ASP.NET Core defaults
+                //the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Access/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddMvc();
         }
 
@@ -37,6 +71,7 @@ namespace BookCave
             }
 
             app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
