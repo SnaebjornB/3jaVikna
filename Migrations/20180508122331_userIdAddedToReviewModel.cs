b491398bea8303bcf04f404e83a1f48edeb90057
Benedikt Runar <benedikt17@ru.is>
2018-05-08 16:48:05 +0000

    userID bætt við Reviews í gagnagrunni

---
+++ Migrations/20180508122331_userIdAddedToReviewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class userIdAddedToReviewModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userID",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userID",
+                table: "Reviews");
+        }
+    }
+}
--- Models/EntityModels/ReviewEntity.cs
+++ Models/EntityModels/ReviewEntity.cs
@@ -3,6 +3,7 @@ namespace BookCave.Models.EntityModels
     public class ReviewEntity{
         public int ID { get; set; }
         public int BookID { get; set; }
+        public string userID { get; set; }
         public string username { get; set; }
         public string review { get; set; }
         public double rating { get; set; }
